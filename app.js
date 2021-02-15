0723f9186e819c546ac78a0185da8e66ba9bd374
AntsmithCode <hey@antsmith.dev>
2021-02-15 07:44:32 -0500

    App edits

--- app.js
+++ app.js
@@ -1,63 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors"); // add cors
-const pokeId = 150;
 const path = require("path");
 const web = path.join(__dirname, "web");
 const fetch = require("node-fetch"); // npm install node-fetch
-const port = 5000;
+const port = 4000;
 //const jsdom = require("jsdom");
 //const { JSDOM } = jsdom;
 
 //global.document = new JSDOM(html).window.document;
 
-const pokeColors = {
-  fire: "#FDDFDF",
-  grass: "#DEFDE0",
-  electric: "#FCF7DE",
-  water: "#DEF3FD",
-  ground: "#f4e7da",
-  rock: "#d5d5d4",
-  fairy: "#fceaff",
-  poison: "#98d7a5",
-  bug: "#f8d5a3",
-  dragon: "#97b3e6",
-  psychic: "#eaeda1",
-  flying: "#F5F5F5",
-  fighting: "#E6E0D4",
-  normal: "#F5F5F5",
-};
-
 app.use(cors());
+app.use(express.static("web"));
 
 app.get("/Pokemon", function (req, res) {
   res.sendFile(path.join(web, "index.html"));
 });
 
-const allPokemon = async () => {
-  for (let i = 1; i <= pokeId; i++) {
-    await getPokemon(i);
-  }
-};
-
-const getPokemon = async (id) => {
-  var requestUri = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  fetch(requestUri)
-    .then((response) => {
-      return response.json();
-    })
-    .then((pokemon) => {
-      const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-      const types = pokemon.types.map((type) => type.type.name).join(", ");
-      const sprites = pokemon.sprites.front_default;
-      console.log(`Name: ${name}\nTypes: ${types}\nSprites: ${sprites}`);
-      createPokemonCard(pokemon);
-    });
-};
-
-allPokemon();
-
-app.use(express.static("web"));
+app.get("/Pokemon/:id", async (req, res) => {
+  let pokeNum = req.params.id;
+  let resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`);
+  let data = await resp.json();
+  console.log(data);
+  res.send(data);
+});
 
 app.listen(port, () => {
   console.log(`Pokemon app listening at http://localhost:${port}`);
--- web/index.js
+++ web/index.js
@@ -1,7 +1,33 @@
-const localhost = "http://localhost:5000/pokemon/";
+const allPokemon = async () => {
+  for (let i = 1; i <= 150; i++);
 
-async function allPoke() {
-  const response = await fetch(localhost);
-  const json = await response.json();
-  console.log(json.name);
-}
+  const getPokemon = async (i) => {
+    pokeElement.innerHTML = "";
+    var requestUri = "http://localhost:4000/pokemon/" + i;
+    fetch(requestUri)
+      .then((response) => {
+        return response.json();
+      })
+      .then((pokemon) => {
+        const name =
+          pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        const types = pokemon.types.map((type) => type.type.name).join(", ");
+        const sprites = pokemon.sprites.front_default;
+        const pokeColors = pokeColors[types];
+        const pokeTemplate = { name, types, sprites, pokeColors };
+
+        json.pokemon.forEach(pokeTemplate);
+        pokeElement.innerHTML += `
+                            <div>
+                            <h3>${name}</h3>
+                            <h4>${pokemon.id.toString().padStart(3, "0")}
+                                </h4>
+                            <h5>${types}</h5>
+                            <h6>color</h6>
+                            <img src="${sprites}">
+                        </div>
+                        `;
+      });
+  };
+  getPokemon();
+};
