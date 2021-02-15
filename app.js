d0061a2e05313927c4547b3a150e764465461178
Anthony Smith <phreedev@Anthonys-MacBook-Air.local>
2021-02-15 16:33:51 -0500

    moving code to windows

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors"); // add cors
-const path = require("path");
-const web = path.join(__dirname, "web");
+//const path = require("path");
+//const web = path.join(__dirname, "web");
 const fetch = require("node-fetch"); // npm install node-fetch
 const port = 4000;
 //const jsdom = require("jsdom");
@@ -11,18 +11,16 @@ const port = 4000;
 //global.document = new JSDOM(html).window.document;
 
 app.use(cors());
-app.use(express.static("web"));
+//app.use(express.static("web"));
 
-app.get("/Pokemon", function (req, res) {
-  res.sendFile(path.join(web, "index.html"));
-});
+//app.get("/Pokemon", function (req, res) {
+//  res.sendFile(path.join(web, "index.html"));
+//});
 
 app.get("/Pokemon/:id", async (req, res) => {
   let pokeNum = req.params.id;
-  let resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`);
-  let data = await resp.json();
-  console.log(data);
-  res.send(data);
+  let pokeResp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`);
+  res.send(pokeResp.data);
 });
 
 app.listen(port, () => {
--- web/index.js
+++ web/index.js
@@ -1,33 +1,14 @@
-const allPokemon = async () => {
-  for (let i = 1; i <= 150; i++);
 
-  const getPokemon = async (i) => {
-    pokeElement.innerHTML = "";
-    var requestUri = "http://localhost:4000/pokemon/" + i;
-    fetch(requestUri)
-      .then((response) => {
-        return response.json();
-      })
-      .then((pokemon) => {
-        const name =
-          pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-        const types = pokemon.types.map((type) => type.type.name).join(", ");
-        const sprites = pokemon.sprites.front_default;
-        const pokeColors = pokeColors[types];
-        const pokeTemplate = { name, types, sprites, pokeColors };
 
-        json.pokemon.forEach(pokeTemplate);
-        pokeElement.innerHTML += `
-                            <div>
-                            <h3>${name}</h3>
-                            <h4>${pokemon.id.toString().padStart(3, "0")}
-                                </h4>
-                            <h5>${types}</h5>
-                            <h6>color</h6>
-                            <img src="${sprites}">
-                        </div>
-                        `;
-      });
-  };
-  getPokemon();
+
+
+
+const getPokemon = (start, pokeId) => {
+  const allPokes = [];
+  for (let i = start; i <= pokeId; i++) {
+    allPokes.push(
+      fetch(`http://localhost:4000/pokemon${i}`).then((res) => res.json())
+    );
+  }
+  return Promise.all(allPokes);
 };
